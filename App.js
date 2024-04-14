f41ef7ba96b9610dfaea3f63c7b5d59a4c376b7d
alirezayoom <alirezayoom@gmail.com>
2024-04-14 18:23:19 +0430

    add text input and button

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Add new todo..." />
+        <Button title="ADD TODO" />
+      </View>
+      <View style={styles.line}></View>
+      <View style={styles.todoList}>
+        <Text>TODOs...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "column",
     flex: 1,
-    backgroundColor: "#fff",
+    padding: 32,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    gap: 8,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-between",
+  },
+  line: {
+    width: "100%",
+    height: 1,
+    backgroundColor: "#ccc",
+    marginVertical: 16,
+  },
+  textInput: {
+    flex: 1,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: "#ccc",
+  },
+  todoList: {
+    flex: 2,
   },
 });
